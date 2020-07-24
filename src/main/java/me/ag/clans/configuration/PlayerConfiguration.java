e1b4d116ed7488e633d08bc70751626dab40f0be
Abdera7mane <56tzminecraft@gmail.com>
2020-07-24 01:51:04 +0100

    better invitation handling

--- src/main/java/me/ag/clans/configuration/PlayerConfiguration.java
+++ src/main/java/me/ag/clans/configuration/PlayerConfiguration.java
@@ -1,76 +1,123 @@
 package me.ag.clans.configuration;
 
-import me.ag.clans.ClansPlugin;
-import me.ag.clans.types.Clan;
-import me.ag.clans.types.ClanInvite;
-import me.ag.clans.util.ClanUtilities;
-
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.annotation.Nullable;
+
+import me.ag.clans.ClansPlugin;
+import me.ag.clans.types.Clan;
+import me.ag.clans.types.ClanInvitation;
+import me.ag.clans.util.ClanUtilities;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import org.jetbrains.annotations.NotNull;
 
 public class PlayerConfiguration extends YamlConfiguration {
-    private OfflinePlayer player;
+    private final OfflinePlayer player;
+    public static final File defaultPath;
     private static final ClansPlugin plugin = ClansPlugin.getInstance();
 
-    @Override
-    public void load(@NotNull File file) throws IOException, InvalidConfigurationException {
-        super.load(file);
-        String fileName = file.getName();
-        int pos = fileName.lastIndexOf(".");
-        if (pos > 0) {
-            fileName = fileName.substring(0, pos);
-        }
-        UUID uuid = UUID.fromString(fileName);
-        player = Bukkit.getOfflinePlayer(uuid);
+    static {
+        String childPath = "\\data\\players".replace("\\", File.separator);
+        defaultPath = new File(plugin.getDataFolder(), childPath);
     }
 
-    public void addInvite(@NotNull Clan clan) {
-        List<String> invites = getStringList("invites");
-        invites.add(clan.getName());
-        set("invites", invites);
+    public PlayerConfiguration(OfflinePlayer player) {
+        this.player = player;
     }
-
 
     @Nullable
     public Clan getClan() {
-        String clan = getString("clan");
-        return clan != null ? ClanUtilities.getClan(clan) : null;
+        Clan clan = null;
+        if (this.isString("clan")) {
+            String clanName = this.getString("clan", "");
+            clan = ClanUtilities.getClan(clanName);
+        }
+
+        if (clan == null) {
+            this.setClan(null);
+        }
+
+        return clan;
+    }
+
+    public void setClan(String name) {
+        this.set("clan", name);
     }
 
     public OfflinePlayer getPlayer() {
         return this.player;
     }
 
-    public List<ClanInvite> getInvites() {
-        List<ClanInvite> invites = new ArrayList<>();
-        for (String clan : getStringList("invites")) {
-            invites.add(new ClanInvite(ClanUtilities.getClan(clan), this.player));
+    public Set<ClanInvitation> getInvitations() {
+        Set<ClanInvitation> invites = new HashSet<>();
+
+        for(String clanName : this.getStringList("invites")) {
+            Clan instance = ClanUtilities.getClan(clanName);
+            if (instance != null) {
+                ClanInvitation invitation = new ClanInvitation(instance, this.player);
+                invites.add(invitation);
+            }
         }
+
+        this.setInvitations(invites);
         return invites;
+    }
+
+    public void setInvitations(@NotNull Set<ClanInvitation> invites) {
+        Set<String> list = new HashSet<>();
+
+        for (ClanInvitation invitation : invites) {
+            list.add(invitation.getClan().getName().toLowerCase());
+        }
+
+        this.set("invites", list.size() > 0 ? new ArrayList<>(list) : null);
+    }
+
+    public void addInvitation(@NotNull ClanInvitation invitation) {
+        Set<ClanInvitation> invitations = this.getInvitations();
+        invitations.add(invitation);
+        this.setInvitations(invitations);
+    }
+
+    public void removeInvitation(@NotNull Clan from) {
+        Set<ClanInvitation> list = this.getInvitations();
+        list.remove(new ClanInvitation(from, this.player));
+        this.setInvitations(list);
+    }
+
+    public boolean isInvitedBy(@NotNull Clan clan) {
+        return this.getStringList("invites").contains(clan.getName().toLowerCase());
     }
 
     public void save() throws IOException {
         String uuid = this.player.getUniqueId().toString();
-        String childPath = "\\data\\players\\".replace("\\", File.separator) + uuid + ".yml";
-        File file = new File(plugin.getDataFolder(), childPath);
-        save(file);
+        String childPath = uuid + ".yml";
+        File file = new File(defaultPath, childPath);
+        this.save(file);
     }
 
-    @Override
     public void save(@NotNull File file) throws IOException {
-        if (this.saveToString().length() > 0) {
+        if (this.getKeys(false).size() > 0) {
             super.save(file);
+        } else {
+            file.delete();
         }
+
+    }
+
+    public String toString() {
+        Configuration root = this.getRoot();
+        return this.getClass().getSimpleName()
+                + "[path='" + this.getCurrentPath()
+                + "', root='" + (root == null ? null : root.getClass().getSimpleName())
+                + "', player=" + this.player.getUniqueId() + "]";
     }
 }
