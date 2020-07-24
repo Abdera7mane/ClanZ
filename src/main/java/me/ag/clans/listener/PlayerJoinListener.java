1f37600f044bb8dae9ea49f3b514f32c85633323
Abdera7mane <56tzminecraft@gmail.com>
2020-07-24 01:59:31 +0100

    load/unload PlayerConfiguration & ClanConfiguration

--- src/main/java/me/ag/clans/listener/PlayerJoinListener.java
+++ src/main/java/me/ag/clans/listener/PlayerJoinListener.java
@@ -1,9 +1,9 @@
 package me.ag.clans.listener;
 
-import me.ag.clans.ClansPlugin;
 import me.ag.clans.configuration.PlayerConfiguration;
 import me.ag.clans.types.Clan;
 import me.ag.clans.util.PlayerUtilities;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,11 +15,9 @@ public class PlayerJoinListener implements Listener {
     private void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         PlayerConfiguration configuration = PlayerUtilities.getPlayerConfiguration(player);
-        ClansPlugin.cachePlayer(configuration, false);
         Clan clan = configuration.getClan();
         if (clan != null) {
-            ClansPlugin.log("loaded clan: " + clan);
-            ClansPlugin.cacheClan(clan, false);
+            player.sendMessage("your clan is " + clan.getName());
         }
     }
 }
--- src/main/java/me/ag/clans/listener/PlayerQuitListener.java
+++ src/main/java/me/ag/clans/listener/PlayerQuitListener.java
@@ -1,10 +1,12 @@
 package me.ag.clans.listener;
 
+import java.io.IOException;
+
 import me.ag.clans.ClansPlugin;
 import me.ag.clans.configuration.PlayerConfiguration;
 import me.ag.clans.types.Clan;
-import me.ag.clans.types.ClanMember;
 import me.ag.clans.util.PlayerUtilities;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,15 +18,24 @@ public class PlayerQuitListener implements Listener {
     private void onLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         PlayerConfiguration configuration = PlayerUtilities.getPlayerConfiguration(player);
-        ClansPlugin.cachePlayer(configuration);
+
+        try {
+            configuration.save();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ClansPlugin.cachePlayer(configuration, true);
         Clan clan = configuration.getClan();
         if (clan != null) {
-            for (ClanMember member : clan.getMembers()) {
-                if (member.getPlayer().isOnline()) return;
-            }
-            ClansPlugin.log("caching " + clan);
-            ClansPlugin.cacheClan(clan);
-        }
-    }
+            ClansPlugin.cacheClan(clan, true);
 
+            try {
+                clan.save();
+            } catch (IOException var6) {
+                var6.printStackTrace();
+            }
+        }
+
+    }
 }
