a1251edb5eab7152f9a4dffb7c14de1bb040e240
Abdera7mane <56tzminecraft@gmail.com>
2020-07-24 02:30:47 +0100

    fix #getClan where it shouldn't return null

--- src/main/java/me/ag/clans/util/ClanUtilities.java
+++ src/main/java/me/ag/clans/util/ClanUtilities.java
@@ -1,60 +1,77 @@
 package me.ag.clans.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.Nullable;
+
 import me.ag.clans.ClansPlugin;
 import me.ag.clans.configuration.ClanConfiguration;
-import me.ag.clans.configuration.PlayerConfiguration;
+import me.ag.clans.configuration.InvalidClanConfigurationException;
 import me.ag.clans.events.ClanCreateEvent;
 import me.ag.clans.types.Clan;
+import me.ag.clans.types.ClanMember;
+
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.InvalidConfigurationException;
 
-import javax.annotation.Nullable;
-import java.io.File;
-import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
 
 public class ClanUtilities {
     private static final ClansPlugin plugin = ClansPlugin.getInstance();
 
-    public static boolean createClan(String name, OfflinePlayer leader) {
+    public static boolean createClan(@NotNull String name, @NotNull OfflinePlayer leader) {
         Clan clan = new Clan(name, leader);
         ClanCreateEvent event = new ClanCreateEvent(clan);
         plugin.getPluginManager().callEvent(event);
         if (event.isCancelled()) {
             return false;
         }
-        PlayerConfiguration playerConfig = ClansPlugin.getPlayerCache(leader);
-        playerConfig.set("clan", name);
-        clan.save();
 
         try {
-            playerConfig.save();
-
+            clan.save();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        ClansPlugin.cacheClan(clan);
+        ClansPlugin.cacheClan(clan, !leader.isOnline());
         return true;
+
     }
+
     @Nullable
     public static Clan getClan(String name) {
         Clan clan = null;
         if (ClansPlugin.isClanCached(name)) {
             clan = ClansPlugin.getClanCache(name);
         } else {
-            String childPath = "\\data\\clans\\".replace("\\", File.separator) + name + ".yml";
-            File file = new File(plugin.getDataFolder(), childPath);
-            if (!file.isFile()) {
-                return null;
-            }
-            ClanConfiguration configuration = new ClanConfiguration();
+            String childPath = File.separator + name.toLowerCase() + ".yml";
+            File file = new File(ClanConfiguration.defaultPath, childPath);
+
             try {
+                ClanConfiguration configuration = new ClanConfiguration();
                 configuration.load(file);
-                clan = new Clan(name, configuration);
-                ClansPlugin.cacheClan(clan);
-            } catch (IOException | InvalidConfigurationException ignored) {}
+                clan = new Clan(configuration);
+            } catch (InvalidConfigurationException | InvalidClanConfigurationException | IOException ignored) {}
+        }
+
+        if (clan != null) {
+            boolean expire = true;
+
+            for (ClanMember member : clan.getMembers()) {
+                if (member.getPlayer().isOnline()) {
+                    expire = false;
+                    break;
+                }
+            }
+
+            ClansPlugin.cacheClan(clan, expire);
         }
 
         return clan;
+    }
+
+    public static boolean isValidClanName(@NotNull String name) {
+        return name.matches("[a-zA-Z0-9]+") && name.length() > 3 && name.length() < 12;
     }
 }
