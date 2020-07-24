50587725c69b90ec39895d27bf2d237b8320f685
Abdera7mane <56tzminecraft@gmail.com>
2020-07-24 01:52:54 +0100

    add InvalidClanConfigurationException

---
+++ src/main/java/me/ag/clans/configuration/InvalidClanConfigurationException.java
@@ -0,0 +1,7 @@
+package me.ag.clans.configuration;
+
+public class InvalidClanConfigurationException extends Exception {
+    public InvalidClanConfigurationException(String message) {
+        super(message);
+    }
+}
