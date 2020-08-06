78156795fefc863fff11cfa2b1dd9757f6400542
Abdera7mane <56tzminecraft@gmail.com>
2020-08-06 14:36:32 +0100

    add basic reload functionality

--- src/main/java/me/ag/clans/commands/subcommands/ReloadCommand.java
+++ src/main/java/me/ag/clans/commands/subcommands/ReloadCommand.java
@@ -1,5 +1,6 @@
 package me.ag.clans.commands.subcommands;
 
+import me.ag.clans.ClansPlugin;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
@@ -8,17 +9,31 @@ import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand extends SubCommand{
+
     public ReloadCommand() {
         super("reload", new ArrayList<>(Arrays.asList("rl")));
     }
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, String[] args) {
-        return false;
+        ClansPlugin plugin = ClansPlugin.getInstance();
+        boolean success = plugin.loadConfig();
+        if (success) {
+            sender.sendMessage("§2configuration reloaded !");
+        }
+        else {
+            sender.sendMessage("§4Couldn't reload plugin's configuration, please check the logs");
+        }
+        return true;
     }
 
     @Override
     public boolean isPlayerCommand() {
+        return false;
+    }
+
+    @Override
+    public boolean clanRequired() {
         return false;
     }
 }
