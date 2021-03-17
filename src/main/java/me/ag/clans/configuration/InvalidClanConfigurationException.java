57455f658c09efc367659d87039c432808002b14
Abdera7mane <56tzminecraft@gmail.com>
2021-03-17 21:05:57 +0100

    cleanup

--- src/main/java/me/ag/clans/configuration/InvalidClanConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package me.ag.clans.configuration;
-
-public class InvalidClanConfigurationException extends Exception {
-    public InvalidClanConfigurationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/me/ag/clans/util/InvalidClanNameException.java
+++
@@ -1,7 +0,0 @@
-package me.ag.clans.util;
-
-public class InvalidClanNameException extends Exception {
-    public InvalidClanNameException(String message) {
-        super(message);
-    }
-}
