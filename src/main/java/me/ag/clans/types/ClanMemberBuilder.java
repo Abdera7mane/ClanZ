aebf0de6ea533c1263e8017a1263837f3e8be004
Abdera7mane <56tzminecraft@gmail.com>
2021-03-17 21:13:03 +0100

    add Builder for the known types

---
+++ src/main/java/me/ag/clans/types/ClanMemberBuilder.java
@@ -0,0 +1,60 @@
+package me.ag.clans.types;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+import java.util.Date;
+import java.util.UUID;
+
+@SuppressWarnings({"unused", "UnusedReturnValue"})
+public class ClanMemberBuilder {
+    private final OfflinePlayer offlinePlayer;
+    private ClanRole role;
+    private int kills;
+    private Date joinDate;
+
+    public ClanMemberBuilder(UUID playerUUID) {
+        this(Bukkit.getOfflinePlayer(playerUUID));
+    }
+
+    public ClanMemberBuilder(OfflinePlayer player) {
+        this.offlinePlayer = player;
+        this.setRole(ClanRole.MEMBER)
+            .setJoinDate(new Date());
+    }
+
+    public ClanMemberBuilder setRole(ClanRole role) {
+        this.role = role != null ? role : ClanRole.MEMBER;
+        return this;
+    }
+
+    public ClanMemberBuilder setKills(int kills) {
+        this.kills = kills;
+        return this;
+    }
+
+    public ClanMemberBuilder setJoinDate(Date joinDate) {
+        this.joinDate = joinDate != null ? joinDate : new Date();
+        return this;
+    }
+
+    public ClanMember build(Clan clan) {
+        return new ClanMember(clan, this);
+    }
+
+    public OfflinePlayer getOfflinePlayer() {
+        return this.offlinePlayer;
+    }
+
+    public int getKills() {
+        return this.kills;
+    }
+
+    public ClanRole getRole() {
+        return this.role;
+    }
+
+    public Date getJoinDate() {
+        return this.joinDate;
+    }
+}
---
+++ src/main/java/me/ag/clans/types/PlayerAdapterBuilder.java
@@ -0,0 +1,52 @@
+package me.ag.clans.types;
+
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@SuppressWarnings({"unused", "UnusedReturnValue"})
+public class PlayerAdapterBuilder {
+    private final OfflinePlayer player;
+    private Clan clan;
+    private Set<ClanInvitation> invitations = new HashSet<>();
+
+    public PlayerAdapterBuilder(@NotNull OfflinePlayer player) {
+        this.player = player;
+    }
+
+    public PlayerAdapterBuilder setClan(@Nullable Clan clan) {
+        this.clan = clan;
+        return this;
+    }
+
+    public PlayerAdapterBuilder setInvitaions(@NotNull Set<ClanInvitation> invites) {
+        this.invitations = invites;
+        return this;
+    }
+
+    public PlayerAdapterBuilder appendInvitation(@NotNull ClanInvitation invitation) {
+        this.invitations.add(invitation);
+        return this;
+    }
+
+    public OfflinePlayer getSelf() {
+        return this.player;
+    }
+
+    public Clan getClan() {
+        return this.clan;
+    }
+
+    public Set<ClanInvitation> getInvitations() {
+        return this.invitations;
+    }
+
+    public PlayerAdapter build() {
+        return new PlayerAdapter(this);
+    }
+
+}
